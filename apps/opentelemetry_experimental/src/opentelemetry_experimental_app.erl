781fd910cbfef98d49f056bc73639914b3326514
Serge Tupchii <serge.tupchii@protonmail.com>
2023-11-22 22:11:20 +0200

    feat: add `start_default_tracer`, `start_default_metrics` config options

--- apps/opentelemetry_experimental/src/opentelemetry_experimental_app.erl
+++ apps/opentelemetry_experimental/src/opentelemetry_experimental_app.erl
@@ -15,14 +15,17 @@
 -include_lib("opentelemetry_api_experimental/include/otel_meter.hrl").
 
 start(_StartType, _StartArgs) ->
-    Config = otel_configuration:merge_with_os(
+    #{start_default_metrics := StartDefaultMetrics} = Config = otel_configuration:merge_with_os(
                application:get_all_env(opentelemetry_experimental)),
 
     {ok, Pid} = opentelemetry_experimental_sup:start_link(Config),
 
-
-    {ok, _} = start_default_metrics(Config),
-
+    _ = case StartDefaultMetrics of
+            true ->
+                {ok, _} = start_default_metrics(Config);
+            false ->
+                ok
+        end,
    {ok, Pid}.
 
 stop(_State) ->
